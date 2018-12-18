ab785c698a7a083bc3a8d4e954750795af7bdf66
ngockq <qn.khuat@gmail.com>
2018-12-18 12:47:39 +0700

    done

---
+++ js/call.js
@@ -0,0 +1,43 @@
+function get_link() {
+  var vcrx = "https://vcrxtopkidtrial.topica.vn/"
+
+  var room_id = document.getElementById('room_id').value;
+  var link_slide = document.getElementById('link_slide').value;
+  // Input to keep the place holder
+  var my_link = document.getElementById('my_link');
+
+
+  if (room_id.length >0 && link_slide.length>0){
+
+    // Render link
+    var obj = {
+        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTQ1MTA5NjEwLCJleHAiOjE1NDU1MDk2MTB9.4tNOf2av0ttTRnsZ1NEyhtZvTD-eO8ybuox6njRY-CgPRhTKt4qmMjOon52IL7ov3xv141_zcHBHc8yO6GOW3Q",
+        "topic": "Lesson 2",
+        "timeStart": "1543460400",
+        "userId": 2,
+        "userName": " Giao Vien",
+        "userRole": "STUDENT",
+        "meetingId": 2,
+        "email": "sontm2@topica.edu.vn",
+        "video_warmup": "",
+        "link_slide": room_id,
+        "idVcrx": "0",
+        "idRoomVcrx":"",
+        "moodleToken": ""
+    };
+
+    var str = btoa(jQuery.param(obj));
+    var url = vcrx +room_id+"?token=" +str;
+
+
+
+    // COPY the link to clipboard
+    $('#my_link').val(url);
+    my_link.select();
+    document.execCommand("copy");
+
+
+    // Alert
+    $('.message').text("Đã copy link");
+  }
+}
