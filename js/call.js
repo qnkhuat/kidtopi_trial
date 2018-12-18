6f530c7934a7111adbdafdcc9ddfb78e822fc6d7
ngockq <qn.khuat@gmail.com>
2018-12-18 15:11:24 +0700

    done

--- js/call.js
+++ js/call.js
@@ -1,32 +1,53 @@
+
 var date = new Date();
 var hours = date.getHours();
 var minutes = date.getMinutes();
 var seconds = date.getSeconds();
 var timestamp = hours*3600 + minutes*60 + seconds;
 $('#room_id').val(timestamp);
+$('#link_slide').val('https://topkid.topica.vn/topkid_mat/Lesson2.pdf');
+
+var token = login_vcrx();
+
+function login_vcrx(){
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://vcrxapitester.topica.vn/api/user/login",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache",
+      "Postman-Token": "cc5e946f-5331-4402-bfe2-a5565d2c7b70"
+    },
+    "processData": false,
+    "data": "{\"username\":\"ADMIN\",\"password\":\"123456\"}"
+  }
+
+  $.ajax(settings).done(function (response) {
+    return response.result.accessToken;
+  });
+}
 
 
-function get_link() {
+function get_link(role='STUDENT') {
+  // var token = login_vcrx();
   var vcrx = "https://vcrxtopkidtrial.topica.vn/"
 
-
-  
   var room_id = document.getElementById('room_id').value;
   var link_slide = document.getElementById('link_slide').value;
   // Input to keep the place holder
-  var my_link = document.getElementById('my_link');
 
-
-  if (room_id.length >0 && link_slide.length>0){
+  if (room_id.length >0){
 
     // Render link
     var obj = {
-        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTQ1MTA5NjEwLCJleHAiOjE1NDU1MDk2MTB9.4tNOf2av0ttTRnsZ1NEyhtZvTD-eO8ybuox6njRY-CgPRhTKt4qmMjOon52IL7ov3xv141_zcHBHc8yO6GOW3Q",
+        "token": login_vcrx,
         "topic": "Lesson 2",
         "timeStart": "1543460400",
         "userId": 2,
-        "userName": " Giao Vien",
-        "userRole": "STUDENT",
+        "userName": "",
+        "userRole": role,
         "meetingId": room_id,
         "email": "sontm2@topica.edu.vn",
         "video_warmup": "",
@@ -39,15 +60,44 @@ function get_link() {
     var str = btoa(jQuery.param(obj));
     var url = vcrx +room_id+"?token=" +str;
 
-
-
-    // COPY the link to clipboard
-    $('#my_link').val(url);
-    my_link.select();
-    document.execCommand("copy");
-
-
-    // Alert
-    $('.message').text("Đã copy link");
+    return url;
   }
+  else{
+    return '';
+  }
+
 }
+
+
+function render(){
+  var student_link = get_link('STUDENT');
+  var teacher_link = get_link('teacher');
+  var po_link = get_link('PO');
+
+
+  $('#student_input').val(student_link);
+  $('#teacher_input').val(teacher_link);
+  $('#assist_input').val(po_link);
+
+}
+
+function copy_link(input_id){
+  var my_link = document.getElementById(input_id);
+
+  my_link.select();
+  document.execCommand("copy");
+  $('.message').text("Đã copy link");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//
